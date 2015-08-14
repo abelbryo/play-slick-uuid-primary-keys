17693411aefd39447d9bab68ee888c089519fa93
Abel Terefe <abelbryo@gmail.com>
2015-08-14 13:45:07 +0300

    Modified indentation

--- id_generator.sql
+++ id_generator.sql
@@ -9,17 +9,17 @@ DECLARE
     -- schema shard you have - you could pass this as a parameter too
     shard_id int := 1;
 
-BEGIN
-    SELECT nextval('shard_1.global_id_sequence') % 1024 INTO seq_id;
+    BEGIN
+        SELECT nextval('shard_1.global_id_sequence') % 1024 INTO seq_id;
 
-SELECT FLOOR(EXTRACT(EPOCH FROM clock_timestamp()) * 1000) INTO now_millis;
-    result := (now_millis - our_epoch) << 23;
-    result := result | (shard_id << 10);
-    result := result | (seq_id);
-END;
+        SELECT FLOOR(EXTRACT(EPOCH FROM clock_timestamp()) * 1000) INTO now_millis;
+        result := (now_millis - our_epoch) << 23;
+        result := result | (shard_id << 10);
+        result := result | (seq_id);
+    END;
 $$ LANGUAGE PLPGSQL;
 
-select shard_1.id_generator();
+SELECT shard_1.id_generator();
 
 -- ============================================
 -- USAGE
